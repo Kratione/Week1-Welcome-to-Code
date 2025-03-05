d06d366bfd2aa392e94ad059fc9f16209faf8137
Kratione <molaingame@gmail.com>
2025-03-05 09:52:02 +0700

    Submit Quiz Week-1

---
+++ quiz/part3/soal5.js
@@ -0,0 +1,18 @@
+// Problem buatlah bintang seperti berikut
+//hasilnya
+//*
+//**
+//***
+//****
+//*****
+
+let input = "";
+for (let i = 1; i <= 5; i++) {
+    for (let j = 1; j <= i; j++) {
+        input += "*";
+    }
+    input += "\n";
+}
+console.log(input);
+
+// SOAL SELESAI
